b285579b94112be43d0bc6f27dc56c1b05cab876
SAMEER SHRESTHA <Nero0293@outlook.com>
2024-07-25 13:14:14 +0545

    Created a hero section and working on the hero-details section

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
+import { NgFor } from '@angular/common';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrl: './heroes.component.css'
+})
+export class HeroesComponent {
+  hero:Hero = {
+    id:1,
+    name:`Windstorm`,
+    date: new Date()  
+  };
+  heroes = HEROES;
+  selectedHero?: Hero;
+  onSelect(hero:Hero):void{
+    this.selectedHero = hero;
+  } 
+}
+
