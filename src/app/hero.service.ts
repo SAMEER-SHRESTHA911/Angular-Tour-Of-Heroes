0be38a565123a5e2d2a7ae42122cba968781be99
SAMEER SHRESTHA <Nero0293@outlook.com>
2024-07-25 17:25:12 +0545

    Fixed the hero details part as a subscribe method was missing in the logic part

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@angular/core';
-import { HEROES } from './mock-heroes';
-import { Hero } from './hero';
+
 import { Observable, of } from 'rxjs';
+
+import { Hero } from './hero';
+import { HEROES } from './mock-heroes';
 import { MessageService } from './message.service';
 
 @Injectable({
@@ -15,5 +17,10 @@ export class HeroService {
     const heroes = of(HEROES);
     this.messageService.add('HeroService:fetched heroes');
     return heroes;
+  }
+  getHero(id:number):Observable<Hero>{
+    const hero = HEROES.find(h => h.id === id)!;
+    this.messageService.add(`HeroService: fetched hero id = ${id}`);
+    return of(hero);
   }
 }
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
-import { MessageService } from '../message.service';
+// import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -10,12 +11,10 @@ import { MessageService } from '../message.service';
 })
 export class HeroesComponent implements OnInit{
 
-  selectedHero?: Hero;
-
   heroes: Hero[] = [];
 
   constructor(
-    private heroService:HeroService, private messageService : MessageService
+    private heroService:HeroService
   ){}
   
   ngOnInit(): void {
@@ -27,10 +26,6 @@ export class HeroesComponent implements OnInit{
   this.heroService.getHeroes()
   .subscribe(heroes => this.heroes= heroes);
 }
-  onSelect(hero:Hero){
-    this.selectedHero = hero;
-    this.messageService.add(`HeroesComponent: Selected hero id = ${hero.id}`)
-  }
   hero:Hero = {
     id:1,
     name:`Windstorm`,
